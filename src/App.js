02daff2c00f8e856adb39b7807a49c7df184cb29
Lisa Bär <lisa.baer@lifebonus.health>
2022-08-09 12:43:41 +0200

    centered cards and added styling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ import Card from '../src/Components/Card';
 function App() {
   return (
     <div className="App">
-      
-        <p>
-          <Card title="People" text="find your favorite people here!"/>
-        </p>
+      <div className="container center-screen">
+        <div className="item">
+          <Card title="People" text="Find your favorite people here!"/>
+        </div>
+        <div className="item">
+        <Card title="Planets" text="All the Planets from Star Wars Universe"/>
+        </div>
+        <div className="item">
+        <Card title="Films" text="All your  favorite Movies"/>
+        </div>
+      </div>
+        
       
     </div>
   );
