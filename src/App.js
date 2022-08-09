7a76fec272eb91c6acb906ff517931a7778b3b31
Lisa Bär <lisa.baer@lifebonus.health>
2022-08-09 15:54:19 +0200

    changed background, changed card design, added headline

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import People from '../src/Components/People';
 import Card from '../src/Components/Card';
+import header from '../src/img/space-moon.jpg'
 
 function App() {
   return (
     <div className="App">
+      <div className="Header">
+        <p className="header-text">Star Wars</p>
+        <p className="header-text header-subline">Put in a catchy headline here!</p>
+        
+      </div>
       <div className="container center-screen">
         <div className="item">
           <Card title="People" text="Find your favorite people here!"/>
@@ -17,7 +23,7 @@ function App() {
         <Card title="Films" text="All your  favorite Movies"/>
         </div>
       </div>
-        
+        <img className="header-img" src={header}/>
       
     </div>
   );
