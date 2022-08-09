88e6a605850e5d4676c0e9a6b021a3dd0128e3d4
Lisa Bär <lisa.baer@lifebonus.health>
2022-08-09 16:14:22 +0200

    adjusted headline

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="Header">
-        <p className="header-text">Star Wars</p>
+        <p className="header-text header-headline">Star Wars</p>
         <p className="header-text header-subline">Put in a catchy headline here!</p>
         
       </div>
