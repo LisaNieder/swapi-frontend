15e94fe4fe9e78901ae2075d250a3d38fa5ca457
Lisa Nieder <lisa.nieder@lifebonus-bgm.com>
2022-08-18 21:54:51 +0200

    setup routing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,4 @@
 import '../App.css';
-import Card from '../Components/Card';
-import backgroundPlanetCard from '../img/background-moon.jpg';
-import backgroundPeopleCard from '../img/background-people.jpg';
-import backgroundMovieCard from '../img/background-camera.jpg';
 
 function Header() {
     return (
@@ -12,19 +8,6 @@ function Header() {
             <p className="header-text header-subline">Put in a catchy headline here!</p>
         
             </div> 
-            <div className="App">
-                <div className="container center-screen">
-                    <div className="item">
-                        <Card title="People" text="Find your favorite people here!" background={backgroundPeopleCard}/>
-                    </div>
-                    <div className="item">
-                        <Card title="Planets" text="All the Planets from Star Wars Universe" background={backgroundPlanetCard}/>
-                    </div>
-                    <div className="item">
-                        <Card title="Films" text="All your  favorite Movies" background={backgroundMovieCard}/>
-                    </div>
-                </div>   
-            </div>
         </div>
         
       
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,23 +1,46 @@
 import '../App.css';
+import Card from '../Components/Card';
+import backgroundPlanetCard from '../img/background-moon.jpg';
+import backgroundPeopleCard from '../img/background-people.jpg';
+import backgroundMovieCard from '../img/background-camera.jpg';
 import People from '../Components/People';
 import Planets from '../Components/Planets';
 import Films from '../Components/Films';
+import Home from '../Components/Home';
 import {Routes, Route, 
-    BrowserRouter as Router, Redirect, withRouter} from 'react-router-dom';
+    BrowserRouter as Router, Redirect, withRouter, Link} from 'react-router-dom';
 import React from 'react';
 
 function Main() {
     return(
         <React.Fragment>
-           
-            <div className="container center-screen">             
-                <Router>
+            <Router>
+            <div className="container center-screen">
+                    <div className="item">
+                        <Link to="/people"><Card title="People" text="Find your favorite people here!" background={backgroundPeopleCard}/></Link>
+                    </div>
+                    <div className="item">
+                        <Link to="/planets"><Card title="Planets" text="All the Planets from Star Wars Universe" background={backgroundPlanetCard}/></Link>
+                    </div>
+                    <div className="item">
+                        <Link to="/films"><Card title="Films" text="All your  favorite Movies" background={backgroundMovieCard}/></Link>
+                    </div>
+                </div> 
+                       
+                <div className="container center-screen">
                 <Routes>
+                    <Route exact path='/' element={<Home/>}/>
                     <Route path='/people' element={<People/>} />
+                    <Route path='/planets' element={<Planets/>} />
+                    <Route path='/films' element={<Films/>} />
                     
                 </Routes>
+                </div>
             </Router>
-            </div>
+            
+
+            
+            
         </React.Fragment>
     );
 }
